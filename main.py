d7256bfd4bad3680535e87879e4c7e39fa46bcea
Sivaraj <sivaraj.ambat@gmail.com>
2025-07-07 01:05:50 +0530

    Second Commit

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message":"Hello World"}
+
+@app.get("/hello/{name}")
+def say_hello(name:str):
+    return {"message": f"Hello, {name}!"}
+    
